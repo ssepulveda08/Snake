3f90c9af28f9ccbc7e67a64f30f704aa1b207a5f
Santiago Andres Sepulveda <santiago.sc08@gmail.com>
2018-03-15 23:21:47 -0500

    update app

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
   res.render('snake.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
